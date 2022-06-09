2cf752fe76f2ddcd58b8273795e5cb007ca6f829
lgf2111 <lgf2111@gmail.com>
2022-06-09 10:35:14 +0800

    sensitive data exposure

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,7 +2,7 @@ import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+# from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 import logging
@@ -23,7 +23,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'd4c39371a9cfbee4c7f47cad1979a0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+# bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, current_user, logout_user, login_required
-from webapp import app, bcrypt, db
+# from webapp import app, bcrypt, db
+from webapp import app, db
 from webapp.forms import LoginForm, RegistrationForm
 from webapp.models import User
 
@@ -15,8 +16,9 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        # hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        # user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in.', 'success')
@@ -30,7 +32,8 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        # if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and user.password == form.password.data:
             login_user(user)
             flash('Login Successful.', 'success')
             app.logger.info(f"{user.username} logged in successfully.")
@@ -50,6 +53,12 @@ def logout():
 @login_required
 def account():
     return render_template('account.html', title='Account')
+
+@app.route("/admin/dashboard")
+@login_required
+def admin_dashboard():
+    users = User.query.all()
+    return "".join(_.password for _ in users)
 
 @app.route('/debug-sentry')
 def trigger_error():
