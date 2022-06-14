5d319d8e8fa607c4cd28ef1a60d733f2fdd2bf6d
lgf2111 <lgf2111@gmail.com>
2022-06-14 22:22:19 +0800

    successfully implemented flaskblog

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from webapp import app
+from flaskblog import create_app
+from dotenv import load_dotenv
+
+app = create_app()
 
 if __name__ == '__main__':
+    load_dotenv()
     app.run(debug=True)
