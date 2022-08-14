14f057777de8c556a3c5f279ea8164c590e32563
Chickhao <cheehaoong479@gmail.com>
2022-08-14 21:42:57 +0800

    added one thing sorry

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,11 +1,18 @@
 from flask import render_template, request, Blueprint
 from flaskblog.models import Post
+from flask_csp.csp import csp_header
 
 main = Blueprint('main', __name__)
+
+@main.after_request
+def add_security_headers(resp):
+    resp.headers['Content-Security-Policy']="script-src 'self'"
+    return resp
 
 
 @main.route("/")
 @main.route("/home")
+@csp_header({'script-src':"'none'"})
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
